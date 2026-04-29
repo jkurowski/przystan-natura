76f438e5c0ca5c4f68faa476f4191d80b208f70c
Jacek Kurowski <jacek@4dl.pl>
2026-04-29 22:04:48 +0200

    [Jacek][Fix] RWD menu

--- app/Http/Controllers/Front/Developro/InvestmentHouseController.php
+++ app/Http/Controllers/Front/Developro/InvestmentHouseController.php
@@ -9,31 +9,13 @@ use App\Models\RodoRules;
 
 class InvestmentHouseController extends Controller
 {
-    public function index($slug, Property $property)
+    public function index(Property $property)
     {
-        $investment = Investment::findBySlug($slug);
-
-        // Check if the property exists in the investment's properties
-        if (!$investment->properties->contains($property)) {
-            abort(404, 'Property not found in the specified investment.');
-        }
-
-        $areaSearch = $property->area_search;
-
-        $similarProperties = Property::where('id', '!=', $property->id)
-            ->where('investment_id', '=', $investment->id)
-            ->where('status', '=', 1)
-            ->inRandomOrder()
-            ->take(5)
-            ->get();
-
         return view('front.developro.investment_house.index', [
-            'investment' => $investment,
             'property' => $property,
-            'next' => $property->findNext($investment->id, $property->number_order),
-            'prev' => $property->findPrev($investment->id, $property->number_order),
-            'rules' => RodoRules::orderBy('sort')->whereActive(1)->get(),
-            'similarProperties' => $similarProperties
+            'next' => $property->findNext(1, $property->number_order),
+            'prev' => $property->findPrev(1, $property->number_order),
+            'rules' => RodoRules::orderBy('sort')->whereActive(1)->get()
         ]);
     }
 }
