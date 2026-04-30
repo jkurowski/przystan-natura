2153b046151a3085485544ee5c825a42e3736180
Jacek Kurowski <jacek@4dl.pl>
2026-04-30 21:11:07 +0200

    [Jacek][Fix] Karta domu

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -24,7 +24,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=1.0.8" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v=1.0.9" />
 
     <!-- Preloads -->
     <link rel="preload" href="{{ asset('images/SVG/logo.svg') }}" as="image" />
