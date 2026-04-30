b3093c6742ae375700b5c3a1d8fb417dfa9b5d12
Jacek Kurowski <jacek@4dl.pl>
2026-04-30 22:05:30 +0200

    [Jacek][Fix] Karta domu

--- app/Http/Controllers/Front/Developro/InvestmentHouseController.php
+++ app/Http/Controllers/Front/Developro/InvestmentHouseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front\Developro;
 
 use App\Http\Controllers\Controller;
 use App\Models\Investment;
+use App\Models\Page;
 use App\Models\Property;
 use App\Models\RodoRules;
 
@@ -12,9 +13,12 @@ class InvestmentHouseController extends Controller
     public function index(Property $property)
     {
         $investment = Investment::find(1);
+        $page = Page::where('id', 5)->first();
+
         return view('front.developro.investment_house.index', [
             'investment' => $investment,
             'property' => $property,
+            'page' => $page,
             'next' => $property->findNext(1, $property->number_order),
             'prev' => $property->findPrev(1, $property->number_order),
             'rules' => RodoRules::orderBy('sort')->whereActive(1)->get()
