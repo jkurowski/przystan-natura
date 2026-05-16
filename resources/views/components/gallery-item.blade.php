6f3404ebc057cc40f3a157fa9239a77bf1f18ffe
Jacek <kontakt@jk-design.pl>
2026-05-16 15:16:27 +0200

    [Jacek][Fix] Popup obrazków w galerii

--- resources/views/components/gallery-item.blade.php
+++ resources/views/components/gallery-item.blade.php
@@ -1,5 +1,5 @@
 <div class="col-12 col-sm-6 col-lg-4 mb-4">
-    <a href="{{ asset('uploads/gallery/images/' . $image->file) }}" class="swipebox">
+    <a href="{{ asset('uploads/gallery/images/' . $image->file) }}" class="swipebox" rel="gallery">
         <picture>
             <source srcset="{{ asset('uploads/gallery/images/thumbs/webp/' . $image->file_webp) }}" type="image/webp">
             <img src="{{ asset('uploads/gallery/images/thumbs/' . $image->file) }}" alt="{{ $image->name ?? '' }}">
